544a1e380373bb6a9e69ee06f15c27ce7953e406
cyh86086-alt <cyh86086@gmail.com>
2026-07-27 11:38:18 +0800

    feat: implement portable image input core

--- src/core/index.js
+++ src/core/index.js
@@ -1,2 +1,10 @@
 export { validateAndroidImageUri } from "./image-reference.js";
+export {
+  detectImageMimeType,
+  encodeBytesToBase64,
+  IMAGE_INPUT_ERROR_CODES,
+  ImageInputError,
+  normalizeMimeType,
+  prepareImageInput,
+} from "./image-input.js";
 export { validateVisionMetadata } from "./metadata.js";
